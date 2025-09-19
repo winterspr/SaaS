48bb0aae6a36cbb97acb96c8ef4047e6e8470c18
winterspr <xuandong872003@gmail.com>
2025-09-19 21:54:39 +0700

    19/9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
-      hello world
-    </div>
+    <Button variant={"default"} size="lg">
+      Click Me
+    </Button>
   );
 }
